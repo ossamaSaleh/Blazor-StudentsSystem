dab230bc8ca23143f15b02e19b2964c0269bbead
ossamaSaleh <44745271+ossamaSaleh@users.noreply.github.com>
2021-01-10 02:33:51 +0400

    Add files via upload

---
+++ StudentSystem.Infra/Controllers/Student.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StudentSystem.Infra.Data.Db;
+using StudentSystemServer.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSystem.Infra.Controllers
+{
+    [ApiController]
+    public class Student : Controller
+    {
+        private readonly SchoolContext _context;
+        public Student(SchoolContext Context)
+        {
+            _context = Context;
+        }
+        [HttpGet]
+        [Route("api/Students/GetAll")]
+        public async Task<List<StudentProfileDTO>> GetAll()
+        {
+            var result = await _context.StudentGeneralProfile.AsNoTracking().ToListAsync();
+            List<StudentProfileDTO> lstObj = new List<StudentProfileDTO>();
+            foreach (var item in result)
+            {
+                StudentProfileDTO obj = new StudentProfileDTO();
+                obj.Birthdate = item.Birthdate;
+                obj.FirstName = item.FirstName;
+                obj.Gender = item.Gender;
+                obj.LastName = item.LastName;
+                obj.Mobile = item.Mobile;
+                obj.ParentMobile1 = item.ParentMobile1;
+                obj.ParentMobile2 = item.ParentMobile2;
+                lstObj.Add(obj);
+            }
+            return lstObj;
+        }
+    }
+}
---
+++ StudentSystem.Shared/StudentProfileDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StudentSystemServer.Shared
+{
+    public class StudentProfileDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string ParentMobile1 { get; set; }
+        public string ParentMobile2 { get; set; }
+        public bool Gender { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string FullName { get { return this.FirstName + " " + this.LastName; } }
+    }
+}
