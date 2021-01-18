652718379dabf604a2f7f885cd30c9fec08081db
ossamaSaleh <44745271+ossamaSaleh@users.noreply.github.com>
2021-01-19 01:56:06 +0400

    Update Student.cs
    Adding the Save API

--- StudentSystem.Infra/Controllers/Student.cs
+++ StudentSystem.Infra/Controllers/Student.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StudentSystem.Infra.Data.Db;
+using StudentSystem.Infra.SchoolDB;
 using StudentSystemServer.Shared;
 using System;
 using System.Collections.Generic;
@@ -33,9 +35,40 @@ namespace StudentSystem.Infra.Controllers
                 obj.Mobile = item.Mobile;
                 obj.ParentMobile1 = item.ParentMobile1;
                 obj.ParentMobile2 = item.ParentMobile2;
+                obj.Id = item.Id;
                 lstObj.Add(obj);
             }
             return lstObj;
+        }
+        [HttpPost]
+        //[EnableCors("*")]
+        [Route("api/Students/Create")]
+        public async Task<Response<StudentProfileDTO>> CreateNew([FromBody] StudentProfileDTO studentProfile)
+        {
+            Response<StudentProfileDTO> response = new Response<StudentProfileDTO>();
+            var obj = new StudentGeneralProfile();
+            obj.Birthdate = studentProfile.Birthdate;
+            obj.FirstName = studentProfile.FirstName;
+            obj.Gender = studentProfile.Gender;
+            obj.LastName = studentProfile.LastName;
+            obj.Mobile = studentProfile.Mobile;
+            obj.ParentMobile1 = studentProfile.ParentMobile1;
+            obj.ParentMobile2 = studentProfile.ParentMobile2;
+            try
+            {
+                _context.StudentGeneralProfile.Add(obj);
+                _context.SaveChanges();
+                response.Message = "Success";
+                response.Content = studentProfile;
+                response.Id = obj.Id;// _context.StudentGeneralProfile.Select(a => a.Id).LastOrDefault();
+            }
+            catch(Exception e) 
+            {
+                response.Content = studentProfile;
+                response.Message = e.Message;
+            }
+
+            return await Task.FromResult(response);
         }
     }
 }
